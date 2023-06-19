d0d872f4aedf201a1321952086854188e844ae32
plbbowden1 <plbbowden@gmail.com>
2023-06-19 08:09:32 -0400

    Fixed spelling mistake in DashboardHeader

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -11,7 +11,7 @@ const DashboardHeader: React.FC = () => {
   return (
     <header className="d-flex justify-content-between align-items-md-center py-3 m-3 border-bottom">
       <h1 className="h1">
-        <span>The Office: Quote Generator</span>
+        <span>the office: quote generator</span>
       </h1>
       <div>
         <Button
